163d9b85bed1d010a12dbd955f30237b5e863d97
Vahid Alimohamadi <vahid.lid@live.com>
2018-01-17 12:16:37 -0800

    correction of sample tests.

--- changeportto-h3port.py
+++ changeportto-h3port.py
@@ -1,3 +1,3 @@
 from framesnd import sendethframe
 #Should change first MAC (source address) with another host MAC address to simulate movement
-sendethframe('\x00\x00\x00\x00\x00\x01','\x00\x00\x00\x00\x00\x02','\x88\x8F','xx','h3-eth0')
+sendethframe('\x00\x00\x00\x00\x00\x01','\x00\x00\x00\x00\x00\x02','\x88\x8F','yy','h3-eth0')
